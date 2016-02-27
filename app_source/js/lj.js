86a2e938805594c8de60ba44ad9531b04d023e45
cewing <cris@crisewing.com>
2016-02-26 23:57:36 -0800

    updating static files and application js and scss

--- app_source/js/lj.js
+++ app_source/js/lj.js
@@ -1,91 +1,122 @@
-var $ = require('jquery');
+window.$ = require('jquery');
+window.jQuery = window.$;
+var Bootstrap = require('bootstrap');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Lorem = require('react-lorem-component');
+var HomePageContent = module.exports.HomePageContent = require('./modules/lj_pages').HomePageContent;
+var LJEntryList = module.exports.LJEntryList = require('./modules/lj_entries').LJEntryList;
+var LJEntry = module.exports.LJEntry = require('./modules/lj_entries').LJEntry;
 
+window.render_react = (function() {
 
-var Entry = React.createClass({
-
-    getRandomIntInclusive: function(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-
-    getDefaultProps: function() {
-        return {
-            sentenceLowerBound: 7,
-            sentenceUpperBound: 20,
-            paragraphLowerBound: 3,
-            paragraphUpperBound: 10
-        };
-    },
-
-    render: function() {
-        return (
-            <article>
-                <h2>An Entry</h2>
-                <Lorem {...this.props} count={this.getRandomIntInclusive(3, 8)} random={this.getRandomIntInclusive(1, 100000)}/>
-            </article>
-        );
-    }
-})
-
-
-var EntryList = React.createClass({
-    getDefaultProps: function() {
-        return {entryCount: 10};
-    },
-
-    render: function() {
-        var entries = [];
-        for (var i = 0; i < this.props.entryCount; i++) {
-            entries.push(<Entry key={i} />)
+    var inner_render_react = function($containers, cmp_name) {
+        if ($containers === undefined) {
+            $containers = $('[data-react-component]');
         }
-        return (
-            <div>
-                {entries}
-            </div>
-        );
-    }
-});
+        $containers.each(function() {
+            var component_name = cmp_name || $(this).data('react-component');
+            var props = $(this).data('react-props') || {};
 
-var SideBarPanel = React.createClass({
-    render: function() {
-        return (
-            <div className="panel panel-default">
-                <div className="panel-heading">
-                    <h3 className="panel-title">Sidebar Panel</h3>
-                </div>
-                <div className="panel-body">
-                    <ul>
-                        <li><a href="#">A Linked List Entry</a></li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-});
+            var component = module.exports[component_name];
+            if (component === undefined) {
+                throw "Component not defined: " + component_name;
+            }
+            var element = React.createElement(component, props);
+            ReactDOM.render(element, $(this)[0]);
+        });
+    };
 
-var HomePageContent = React.createClass({
-    render: function() {
-        return (
-            <div className="row">
-                <div className="page-header col-sm-12">
-                    <h1>Today I Learned &hellip; <small>401 Python learning Journal</small></h1>
-                </div>
-                <main className="col-sm-9">
-                    <EntryList />
-                </main>
-                <aside className="col-sm-3">
-                  <SideBarPanel />
-                </aside>
-            </div>
-        )
-    }
-});
+    // render react components on page load
+    inner_render_react();
+    return inner_render_react;
 
-ReactDOM.render(
-  <HomePageContent />,
-  document.getElementById('content')
-);
+})();
+
+// var Entry = React.createClass({
+
+//     getRandomIntInclusive: function(min, max) {
+//       return Math.floor(Math.random() * (max - min + 1)) + min;
+//     },
+
+//     getDefaultProps: function() {
+//         return {
+//             sentenceLowerBound: 7,
+//             sentenceUpperBound: 20,
+//             paragraphLowerBound: 3,
+//             paragraphUpperBound: 10
+//         };
+//     },
+
+//     render: function() {
+//         return (
+//             <article>
+//                 <h2>An Entry</h2>
+//                 <Lorem {...this.props} count={this.getRandomIntInclusive(3, 8)} random={this.getRandomIntInclusive(1, 100000)}/>
+//             </article>
+//         );
+//     }
+// })
+
+
+// var EntryList = React.createClass({
+//     getDefaultProps: function() {
+//         return {entryCount: 10};
+//     },
+
+//     render: function() {
+//         var entries = [];
+//         for (var i = 0; i < this.props.entryCount; i++) {
+//             entries.push(<Entry key={i} />)
+//         }
+//         return (
+//             <div>
+//                 {entries}
+//             </div>
+//         );
+//     }
+// });
+
+// var SideBarPanel = React.createClass({
+//     render: function() {
+//         return (
+//             <div className="panel panel-default">
+//                 <div className="panel-heading">
+//                     <h3 className="panel-title">Sidebar Panel</h3>
+//                 </div>
+//                 <div className="panel-body">
+//                     <ul>
+//                         <li><a href="#">A Linked List Entry</a></li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         );
+//     }
+// });
+
+// var HomePageContent = React.createClass({
+//     render: function() {
+//         return (
+//             <div className="row">
+//                 <div className="page-header col-sm-12">
+//                     <h1>Today I Learned &hellip; <small>401 Python learning Journal</small></h1>
+//                 </div>
+//                 <main className="col-sm-9">
+//                     <EntryList />
+//                 </main>
+//                 <aside className="col-sm-3">
+//                   <SideBarPanel />
+//                 </aside>
+//             </div>
+//         )
+//     }
+// });
+
+// ReactDOM.render(
+//   <HomePageContent />,
+//   document.getElementById('content')
+// );
+
+
 
 console.log('i am here');
---
+++ app_source/js/modules/lj_entries.js
@@ -0,0 +1,99 @@
+var React = require('react');
+var moment = require('moment');
+
+var LJPaginator = React.createClass({
+    getDefaultProps: function () {
+        return {paginator: 'foo'}
+    },
+
+    addPaginationMarkup: function() { return {__html: this.props.paginator}; },
+
+    render: function() {
+        var pagination = this.props.paginator;
+        return (
+            <div className="pagination" dangerouslySetInnerHTML={this.addPaginationMarkup()}/>
+        );
+    }
+});
+
+var LJByline = React.createClass({
+    getDefaultProps: function() {
+        return {entry: {}}
+    },
+
+    localizeTime: function(timeString) {
+        var utctime = moment.utc(timeString);
+        var time = moment(utctime.toDate());
+        return time.format('dddd, D MMMM, YYYY, h:mm a');
+    },
+
+    render: function() {
+        var entry = this.props.entry,
+            author = entry ? entry.author : {},
+            display_name = author ? author.display_name : '',
+            user_name = author ? author.user_name : '',
+            author_name = display_name || user_name || 'anonymous',
+            created = entry ? entry.created : '';
+
+        return (
+            <div className="byline">
+                By <span className="author">{author_name}</span> &mdash; {this.localizeTime(created)}
+            </div>
+        );
+    }
+});
+
+var LJEntry = module.exports.LJEntry = React.createClass({
+    getDefaultProps: function() {
+        return {entry: {}, user: {}, title_link: false}
+    },
+
+    getEntryText: function() {
+        var entry = this.props.entry;
+        if (entry.markdown) {
+            return {__html: entry.markdown};
+        } else {
+            return {__html: entry.text};
+        }
+    },
+
+    render: function() {
+        var entry = this.props.entry,
+            title = entry ? entry.title : '',
+            url = entry ? entry.url : '',
+            actions = entry ? entry.actions : {},
+            edit_url = actions ? actions.edit : '',
+            delete_url = actions ? actions.delete : '';
+        return (
+            <article>
+                <h2>
+                    {this.props.title_link ? <a href={url} title={title}>{title}</a> : <span>{title}</span>}
+                </h2>
+                <LJByline entry={entry} />
+                <div className="entryBody" dangerouslySetInnerHTML={this.getEntryText()}/>
+                {edit_url && <a className="btn btn-primary" href={edit_url} title="Edit this entry">Edit</a>}
+                {delete_url && <a className="btn btn-danger pull-right" href={delete_url} title="Delete this entry">Delete</a>}
+            </article>
+        );
+    }
+});
+
+
+var LJEntryList = module.exports.LJEntryList = React.createClass({
+    getDefaultProps: function() {
+        return {entries: [], user: {}, pagination: ''};
+    },
+
+    render: function() {
+        var entries = this.props.entries;
+        var user = this.props.user;
+        var pagination = this.props.pagination;
+        return (
+            <div className="entryList">
+                {(entries.length > 5) && pagination && <LJPaginator paginator={pagination} />}
+                {entries.map(entry => <LJEntry key={entry.id} entry={entry} user={user} title_link={true} />)}
+                {pagination && <LJPaginator paginator={pagination} />}
+            </div>
+        );
+    }
+});
